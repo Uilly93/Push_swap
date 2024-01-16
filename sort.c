d45f722d102e3cb482e891ea33b5bbbcf1939efb
wnocchi <wnocchi@student.42.fr>
2024-01-16 16:27:21 +0100

    sort algo Work In Progress

--- sort.c
+++ sort.c
@@ -6,7 +6,7 @@
 /*   By: wnocchi <wnocchi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 11:56:05 by wnocchi           #+#    #+#             */
-/*   Updated: 2024/01/15 18:47:36 by wnocchi          ###   ########.fr       */
+/*   Updated: 2024/01/16 14:58:29 by wnocchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,13 +71,13 @@ int	ft_max(t_stack *s)
 	}
 	return (max);
 }
-int *lst_last(t_stack *s)
+int lst_last(t_stack *s)
 {
 	t_stack *current;
 	int tmp;
-	
+
 	current = s;
-	tmp = NULL;
+	tmp = 0;
 	while (current)
 	{
 		tmp = current->content;
@@ -86,21 +86,29 @@ int *lst_last(t_stack *s)
 	return(tmp);
 }
 
-int	ft_pos(t_stack *s, int nb)
+int	find_index(t_stack *s, int nb)
+{
+	int i;
+	t_stack
+}
+
+int	find_pos(t_stack *s, int nb)
 {
 	int i;
 	t_stack *current;
 	
 	current = s;
 	i = 1;
-	if (nb > current->content && nb < lst_last(current)
+	if (nb > current->content && nb < lst_last(current))
+		return (0);
 	while(current->next)
 	{
-		if(nb > current->content && nb < current->next->content )
+		if(nb > current->content && nb < current->next->content)
+			break;
 		current = current->next;
 		i++;
 	}
-	return 0;
+	return (i);
 }
 t_stack	*check_closest(t_stack *s, int nb)
 {
@@ -124,20 +132,53 @@ t_stack	*check_closest(t_stack *s, int nb)
 	}
 	return (tmp);
 }
+void	algo_sort(t_stack **a, t_stack **b)
+{
+	t_stack *current;
+	t_stack *current_b;
+	int pos;
 
+	current = *a;
+	current_b = *b;
+	while(current_b)
+	{
+		pos = find_pos(*a, current_b->content);
+		if(pos < lst_size(current) / 2)
+		{
+			while(pos >= 0)
+			{
+				ft_ra(a);
+				pos--;
+			}
+			ft_pa(a, b);
+		}
+		current_b = current_b->next;
+	}
+}
 
-void	sort_five(t_stack **sa, t_stack **sb)
+void	sort_above_four(t_stack **sa, t_stack **sb)
 {
 	t_stack *current;
 	t_stack *current_b;
 
 	current = *sa;
+	int pos;
 	ft_pb(sa, sb);
 	ft_pb(sa, sb);
 	current_b = *sb;
 	sort_three(sa);
-	while(current)
+	while(current_b)
 	{
-		
+		pos = find_pos(*sa, current_b->content);
+		if(pos < lst_size(current) / 2)
+		{
+			while(pos >= 0)
+			{
+				ft_ra(sa);
+				pos--;
+			}
+			ft_pa(sa, sb);
+		}
+		current_b = current_b->next;
 	}
 }
